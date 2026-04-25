ea4833cb65ebce9d62582bd1ae7c7a6fc55be527
yolanqa <yolanda.stefanescu@yahoo.com>
2026-04-25 23:10:34 +0300

    Add saving and loading json

--- storage/file_manager.py
+++ storage/file_manager.py
@@ -1,3 +1,11 @@
 import json
 import csv
 import os
+from models.cafe import Cafea
+
+
+with open("dictionar.json", "w", encoding = "utf-8") as f:
+    json.dump([c.dictionar() for c in coffeeshops], f)
+
+with open("dictionar.json", "r", encoding = "utf-8") as f:
+    dictionar = json.load(f)
