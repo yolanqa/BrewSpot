1bf5ac412d5d1f22f0bcda024ec10fbbd19d2549
yolanqa <yolanda.stefanescu@yahoo.com>
2026-04-30 22:28:41 +0300

    Add code for cafe.py

---
+++ models/cafe.py
@@ -0,0 +1,27 @@
+class Cafe:
+    def __init__(self, name, address, price, program, benefits, rating):
+        self.name = name
+        self.address = address
+        self.price = price
+        self.program = program
+        self.benefits = benefits
+        self.rating = rating
+
+    def __str__(self):
+        return f"Cafenea: {self.name}"
+    
+    def dictionar(slef):
+        d ={"name":self.name,
+            "address": self.address,
+            "price": self.price,
+            "benefits": self.benefits,
+            "rating": self.rating}
+        return d
+    @staticmethod
+    def from_dict(d):
+        cafea = Cafe(name = d["name"], 
+                     address = d["address"],
+                     price = d["price"],
+                     benefits = d["benefits"],
+                     rating = d["rating"])
+        return cafea
--- storage/file_manager.py
+++ storage/file_manager.py
@@ -7,5 +7,8 @@ from models.cafe import Cafea
 with open("dictionar.json", "w", encoding = "utf-8") as f:
     json.dump([c.dictionar() for c in coffeeshops], f)
 
+
+
+
 with open("dictionar.json", "r", encoding = "utf-8") as f:
     dictionar = json.load(f)
